35e2fe58007256ec2073c61193352ae97798d71f
Pieter van Ee <pieter.j.vanee@gmail.com>
2025-10-28 18:35:14 -0400

    Created navigation and main article page.

--- articles/urls.py
+++ articles/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('post_list', views.post_list, name='post_list'),
+    path('main_articles/', views.main_articles, name='main_articles'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def post_list(request):
     return render(request, 'articles/post_list.html', {})
+
+def main_articles(request):
+    return render(request, 'articles/main_articles.html', {})
