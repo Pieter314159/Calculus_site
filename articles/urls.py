272b48e7f60574190eba775733d90c05f5817c72
Pieter van Ee <pieter.j.vanee@gmail.com>
2025-10-30 10:53:43 -0400

    Created form for uploading and editing articles.

--- articles/urls.py
+++ articles/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('post_list', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('article/new/', views.post_new, name='post_new'),
     path('main_articles/', views.main_articles, name='main_articles'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit')
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'articles/post_list.html', {})
+    articles = Article.objects.filter(published_date__lt=timezone.now()).order_by('published_date')
+    return render(request, 'articles/post_list.html', {'articles':articles})
 
 def main_articles(request):
     return render(request, 'articles/main_articles.html', {})
+
+def post_detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    return render(request, 'articles/post_detail.html', {'article':article})
+
+def post_new(request):
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.published_date = timezone.now()
+            article.save()
+            return redirect('post_detail', pk=article.pk)
+    else:
+        form = ArticleForm()
+    return render(request, 'articles/post_edit.html', {'form':form})
+
+def post_edit(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == "POST":
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.published_date = timezone.now()
+            article.save()
+            return redirect('post_detail', pk=article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    return render(request, 'articles/post_edit.html', {"form":form})
